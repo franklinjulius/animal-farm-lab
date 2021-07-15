dbd4e6eb39ef144985c49e5b27f792d61b758e68
Julius Franklin <franklinjulius88@gmail.com>
2021-07-15 15:13:50 -0400

    new animal farm

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-// import functions and grab DOM elements
+const horsePic = document.getElementById('horse-picure');
+const catPic = document.getElementById('cat-picture');
+const dogPic = document.getElementById('dog-picture');
+const horseSound = document.getElementById('horse-sound');
+const catSound = document.getElementById('cat-meow');
+const dogSound = document.getElementById('dog-bark');
 
-// initialize state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+console.log(horseSound);
+console.log(catSound);
+console.log(dogSound);
+
+
+horsePic.addEventListener('click', () => {
+  horseSound.play();
+
+});
+
+catPic.addEventListener('click', () => {
+  catSound.play();
+});
+
+dogPic.addEventListener('click', () => {
+  dogSound.play();
+});
